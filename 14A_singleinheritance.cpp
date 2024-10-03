cd839cc1ccf1997ef02ee66d66eaa226b963fd15
mukeshrothe13 <rothemukesh7@gmail.com>
2024-10-03 20:53:37 +0530

    14A_singleinheritance.cpp

--- 14A_singleinheritance.cpp
+++ 14A_singleinheritance.cpp
@@ -1,1 +1,33 @@
 //Mukesh Rothe 23070123089
+#include <iostream>
+using namespace std;
+class Bands {
+public :
+string bd;
+string sr;
+void genre(){
+    cout<<"Enter the name of band: "<<endl;
+    cin>>bd>>sr;
+}
+};
+class Lz : public
+Bands {
+    public:
+     int est;
+    void estd(){ 
+    cout<<"Estalished: "<<endl;
+    cin>>est;    
+}
+void display(){
+    cout<< "The band is: "<<bd<<" "<<sr<<endl;
+    cout<<"Established: "<<est<<endl;
+}
+};
+int main()
+{
+    Lz d;
+    d.genre();
+    d.estd();
+    d.display();
+    return 0;
+}
